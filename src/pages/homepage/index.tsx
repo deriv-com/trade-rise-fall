9fff22a565e002df4d608301453f136431e791d0
mayuran-deriv <balamayuran@deriv.com>
2025-01-08 20:24:53 +0400

    fix: protect routes

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Button, Heading } from '@deriv-com/quill-ui';
-import { authStore } from '../../stores/AuthStore';
-import './homepage.scss';
+import React from "react";
+import { Button, Heading } from "@deriv-com/quill-ui";
+import { authStore } from "../../stores/AuthStore";
+import { isLogged } from "../../common/utils";
+import "./homepage.scss";
 
 const Homepage: React.FC = () => {
-
   return (
     <div className="homepage">
       <div className="homepage__content">
         <Heading.H1 className="homepage__title">
           Deriv: Option Trading
         </Heading.H1>
-        
+
         <Heading.H3 className="homepage__subtitle">
           Mirror the success of top traders automatically. Set up in minutes
         </Heading.H3>
-        
+
         <Button
-          onClick={() => authStore.login()}
+          onClick={() => {
+            if (isLogged()) {
+              window.location.href = "/dashboard";
+            } else {
+              authStore.login();
+            }
+          }}
           variant="primary"
           size="lg"
         >
